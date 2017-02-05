b65b95eb5492419cbc3ad1dba30ff01b7b308290
Alasdair Reavey <alasdairreavey@gmail.com>
2017-02-05 10:12:20 +0000

    0.1

---
+++ Scripts/MeleeCharacter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeCharacter : MonoBehaviour {
+
+
+    GameObject character;
+    GameObject teamMate;
+    Rigidbody charRB; // Assigned for future use if we do power ups? 
+    Rigidbody teamMateRB;
+    public int teamNumber;
+    bool pickedUp;
+    public int throwForce;
+    float distanceToTeamMate;
+
+    // Use this for initialization
+    void Start ()
+    {
+        pickedUp = false;
+        character = this.gameObject; //sets character gameObject to the object script is attached to
+        charRB = character.GetComponent<Rigidbody>(); //finds rigidbody of the character (may need it later on)
+        teamMate = GameObject.FindGameObjectWithTag("ranged" + teamNumber); //finds tag "ranged1" if teamNumber is set to 1. 
+        teamMateRB = teamMate.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distanceToTeamMate = Vector3.Distance(transform.position, teamMate.transform.position); //calculates the distance between the two players
+
+        if (Input.GetKeyDown("k") && distanceToTeamMate < 1)
+        {
+            pickedUp = true;
+        }
+
+        if (pickedUp)
+        {
+            teamMate.transform.position = new Vector3(character.transform.position.x, character.transform.position.y + 2, character.transform.position.z);
+        }
+
+        if (Input.GetKeyDown("j") && pickedUp)
+        {
+            teamMateRB.AddForce(teamMate.transform.forward * throwForce);
+            teamMateRB.AddForce(teamMate.transform.up * throwForce);
+            pickedUp = false;
+        }
+	}
+}
---
+++ Scripts/MovementScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovementScript : MonoBehaviour {
+
+    GameObject player;
+    Transform mainCamera; //position,rotation of the main camera
+    Vector3 cameraForward; //current forward view of camera
+    Rigidbody playerRB;
+    public float runAcceleration;
+    public float decelerationSlide; // This value controls how much momentum affects deceleration. If 1 - it's as default, if < 1 character slides less, if > 1 character slides more.
+    public float maxRunSpeed;
+    public float jumpHeight;
+    bool grounded;
+    Vector3 move;
+    float turnAmount;
+    Quaternion targetRotation;
+
+    // Use this for initialization
+    void Start ()
+    {
+        targetRotation = transform.rotation;
+        player = this.gameObject;
+        playerRB = player.GetComponent<Rigidbody>();
+        mainCamera = Camera.main.transform;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(playerRB.velocity.magnitude);
+
+        playerRB.velocity = Vector3.ClampMagnitude(playerRB.velocity, maxRunSpeed);
+
+        if ((Input.GetButton("Horizontal") || Input.GetButton("Vertical")) && grounded)
+        {
+            if (move.magnitude > 1f) move.Normalize();
+            move = transform.InverseTransformDirection(move);
+            cameraForward = Vector3.Scale(mainCamera.forward, new Vector3(1, 0, 1)).normalized;
+            move = Input.GetAxis("Vertical") * cameraForward + Input.GetAxis("Horizontal") * mainCamera.right;
+            playerRB.AddForce(move *= 20);
+            //Rotation doesn't work properly. Have to try Lerp. 
+            turnAmount = Mathf.Atan2(move.x, move.z);
+            transform.Rotate(0, turnAmount, 0);
+        }
+        else
+        {
+            playerRB.velocity = new Vector3(playerRB.velocity.x * decelerationSlide, playerRB.velocity.y, playerRB.velocity.z * decelerationSlide); // Have to add time.deltatime calculation for frame differences
+        }
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            playerRB.AddForce(0, jumpHeight, 0);
+        }
+
+        if (!grounded)
+        {
+            //Insert Controls in air
+        }
+	}
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    void OnCollisionExit()
+    {
+        grounded = false;
+    }
+}
