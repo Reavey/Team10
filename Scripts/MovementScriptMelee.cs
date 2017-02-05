a3b18bf14c4ba4078b665f0e520e4c07ceefaee7
Alasdair Reavey <alasdairreavey@gmail.com>
2017-02-05 13:49:24 +0000

    Working Movement, rotation still needs work.

---
+++ Scripts/MovementScriptMelee.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovementScriptMelee : MonoBehaviour {
+    
+    Rigidbody playerRB;
+    public float runAcceleration;
+    public float maxRunSpeed;
+    public float jumpHeight;
+    bool grounded;
+    float turnAmount;
+    Quaternion targetRotation;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerRB = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(playerRB.velocity.magnitude);
+            PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        playerRB.AddForce(Input.GetAxis("Horizontal") * runAcceleration, 0, Input.GetAxis("Vertical") * runAcceleration);
+        transform.LookAt(transform.position + new Vector3(Input.GetAxis("Horizontal") * runAcceleration, 0, Input.GetAxis("Vertical") * runAcceleration));
+        if (transform.rotation != Quaternion.LookRotation(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))))
+        {
+            print(Quaternion.LookRotation(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"))));
+
+        }
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    void OnCollisionExit()
+    {
+        grounded = false;
+    }
+}
---
+++ Scripts/MovementScriptRanged.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScriptRanged : MonoBehaviour {
+
+    Rigidbody playerRB;
+    public float runAcceleration;
+    public float maxRunSpeed;
+    public float jumpHeight;
+    bool grounded;
+    float turnAmount;
+    Quaternion targetRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerRB = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(playerRB.velocity.magnitude);
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        playerRB.AddForce(Input.GetAxis("RightX") * runAcceleration, 0, Input.GetAxis("RightY") * runAcceleration);
+        transform.LookAt(transform.position + new Vector3(Input.GetAxis("RightX") * runAcceleration, 0, Input.GetAxis("RightY") * runAcceleration));
+        if (transform.rotation != Quaternion.LookRotation(new Vector3(Input.GetAxis("RightX"), 0, Input.GetAxis("RightY"))))
+        {
+            print(Quaternion.LookRotation(new Vector3(Input.GetAxis("RightX"), 0, Input.GetAxis("RightY"))));
+
+        }
+    }
+
+    void OnCollisionStay()
+    {
+        grounded = true;
+    }
+
+    void OnCollisionExit()
+    {
+        grounded = false;
+    }
+}
